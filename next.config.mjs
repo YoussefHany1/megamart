6287bcdada3e3c71983096488041e8b44310cb1c
Youssef Hany <youssefhany.2005.yh@gmail.com>
2025-04-13 01:59:23 +0200

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
     images: { unoptimized: true },
-    basePath: '/megamart',
-    assetPrefix: '/megamart/',
 };
 
 export default nextConfig;
